3d370ef71574150e6bd5fdfef0ba27148ef01ba5
Bobholamovic <bob1998425@hotmail.com>
2021-01-16 16:51:59 +0800

    Change `develop` to `install`

--- lib/install.sh
+++ lib/install.sh
@@ -3,6 +3,6 @@
 for module in calc_assoc calc_pixel_feats calc_spixel_feats init_spixel_feats rel_to_abs_index smear
 do
     cd $module
-    python setup.py develop
+    python setup.py install
     cd ..
 done
